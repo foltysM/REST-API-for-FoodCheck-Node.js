f9e2f77e79b019f2f191e5edbe0524a57d581072
foltysM <michal.foltys0@gmail.com>
2020-04-21 17:24:28 +0200

    First commit:

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
 const Joi = require('@hapi/joi');
-//const http = require('http');
+const compression = require('compression');
 const mysql = require('mysql');
 const express = require('express');
+const helmet = require('helmet');
 
 var app = express();
 app.use(express.json());
+app.use(helmet());
+app.use(compression());
 
 const port = process.env.PORT || 3000;
 
 const connection = mysql.createConnection({
-    host: 'hostname',
-    user: 'user',
-    password: 'password',
-    database: 'database'
+    host: 'sql.server153484.nazwa.pl',
+    user: 'server153484_foodcheckapi',
+    password: 'WW7aysAYy5X3ipD',
+    database: 'server153484_foodCheck'
 });
 
+
 app.get('/api/products', function(req, res){
+    if(connection.connect())
+        console.log('Connected with database');
     connection.query('SELECT * FROM food_products', function (error, results, fields) {
         if (error) throw error;
         console.log('GET request for all products');
         if(results.length===0) res.status(404).send('No products in database');
         res.send(JSON.stringify(results));
     });
+    if(connection.end())
+        console.log('Disconnected from database');
 });
 
 app.get('/api/products/:barcode', function(req, res){
+    if(connection.connect())
+        console.log('Connected with database');
     connection.query('SELECT * FROM food_products WHERE barcode = '+req.params.barcode+';', function (error, results, fields) {
         if (error) throw error;
         console.log('GET command for ' + req.params.barcode);
         if(results.length===0) res.status(404).send('Product not found');
         else res.send(JSON.stringify(results));
     });
+    if(connection.end())
+        console.log('Disconnected from database');
 });
 
 app.post('/api/products', function(req, res){
